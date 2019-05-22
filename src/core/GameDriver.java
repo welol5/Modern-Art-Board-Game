775da6fb7486c0153c50445b341ae0fd8694ffb3
Will Elliman <william.elliman@gmail.com>
2019-05-22 11:52:31 -0400

    GameDriver can now setup command line games (does not ask for input);

--- src/core/GameDriver.java
+++ src/core/GameDriver.java
@@ -1,9 +1,34 @@
 package core;
 
+import java.io.IOException;
+
+import io.BasicIO;
+import io.CommandLine;
+
 public class GameDriver implements Runnable{
+	
+	//IO types
+	public static final int COMMAND_LINE = 0;
+	
+	//IO var
+	private BasicIO io;
+	
+	/**
+	 * Setup the program by giving it a IO type.
+	 * IO types are public constant ints in the GameDriver class.
+	 * @param gameType
+	 */
+	public GameDriver(int gameType) throws IOException{
+		if(gameType == COMMAND_LINE) {
+			io = new CommandLine();
+		} else {
+			throw new IOException(gameType + " is not a valid gameType");
+		}
+	}
 
 	@Override
 	public void run() {
+		//need to know how many players
 		
 	}
 	
