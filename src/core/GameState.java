fec93cfd17a525bb4a4a892c43e81fa84b039b31
Will Elliman <william.elliman@gmail.com>
2019-05-22 13:57:44 -0400

    GameState now makes the organized deck

--- src/core/GameState.java
+++ src/core/GameState.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  */
 public class GameState {
 	private Player[] players;
-	private ArrayList<Card> deck;
+	private ArrayList<Card> deck = new ArrayList<Card>();
 	
 	/**
 	 * This is used to setup a new game.
@@ -23,5 +23,95 @@ public class GameState {
 		for(int i = 0; i < players.length; i++) {
 			this.players[i] = new Player(players[i]);
 		}
+		
+		//now the deck needs to be created and shuffled
+		makeDeck();
+	}
+	
+	/**
+	 * This clears and adds all of the default cards to the deck
+	 */
+	private void makeDeck() {
+		deck.clear();
+		
+		//add the Lite Metal paintings
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.SEALED));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.SEALED));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.STANDARD));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.STANDARD));
+		deck.add(new Card(Artist.LITE_METAL,AuctionType.STANDARD));
+		
+		//add the Yoko paintings
+		deck.add(new Card(Artist.YOKO,AuctionType.SEALED));
+		deck.add(new Card(Artist.YOKO,AuctionType.SEALED));
+		deck.add(new Card(Artist.YOKO,AuctionType.SEALED));
+		deck.add(new Card(Artist.YOKO,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.YOKO,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.YOKO,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.YOKO,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.YOKO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.YOKO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.YOKO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.YOKO,AuctionType.STANDARD));
+		deck.add(new Card(Artist.YOKO,AuctionType.STANDARD));
+		deck.add(new Card(Artist.YOKO,AuctionType.STANDARD));
+		
+		//add the Karl Gitter paintings
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.SEALED));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.SEALED));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.SEALED));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.STANDARD));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.STANDARD));
+		deck.add(new Card(Artist.KARL_GITTER,AuctionType.STANDARD));
+		
+		//add the Krypto paintings
+		deck.add(new Card(Artist.KRYPTO,AuctionType.SEALED));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.SEALED));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.SEALED));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.STANDARD));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.STANDARD));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.STANDARD));
+		deck.add(new Card(Artist.KRYPTO,AuctionType.STANDARD));
+		
+		//add the Christin P. paintings
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.SEALED));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.SEALED));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.SEALED));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.DOUBLE));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.ONCE_AROUND));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.FIXED_PRICE));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.STANDARD));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.STANDARD));
+		deck.add(new Card(Artist.CHRISTIN_P,AuctionType.STANDARD));
 	}
 }
