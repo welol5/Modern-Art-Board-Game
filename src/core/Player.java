7bf30ee7f5b397b982cb4ec7c63204d2203d8d66
Will Elliman <william.elliman@gmail.com>
2019-05-22 11:37:56 -0400

    Added constructor to set name and init vars

--- src/core/Player.java
+++ src/core/Player.java
@@ -3,6 +3,13 @@ package core;
 import java.util.ArrayList;
 
 public class Player {
+	private String name;
 	private int money;
 	private ArrayList<Card> hand;
+	
+	public Player(String name) {
+		this.name = name;
+		money = 50;
+		hand = new ArrayList<Card>();
+	}
 }
