ecbbea443b20d14de2a856112a67c5079117d237
Will Elliman <william.elliman@gmail.com>
2019-05-22 11:35:31 -0400

    Added code for getting player names in CommandLine and added comments

--- src/io/BasicIO.java
+++ src/io/BasicIO.java
@@ -1,5 +1,10 @@
 package io;
 
 public abstract class BasicIO {
-
+	/**
+	 * This method should be used to get all of the names of the players in the game.
+	 * The useful part about this is the count of how many players.
+	 * @return the names of all of the players
+	 */
+	public abstract String[] getPlayers();
 }
--- src/io/CommandLine.java
+++ src/io/CommandLine.java
@@ -1,5 +1,30 @@
 package io;
 
-public class CommandLine {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CommandLine extends BasicIO{
+
+	@Override
+	public String[] getPlayers() {
+		String playerName = null;//temporary storage of the current name of a player
+		ArrayList<String> players = new ArrayList<String>();//the names of all of the current players
+		Scanner input = new Scanner(System.in);//the input method for the names
+		System.out.println("Enter the names of the players. When you are done, leave the line blank.");
+		while(true) {
+			System.out.print("Enter a players name: ");
+			playerName = input.nextLine();//get a players name
+			if(playerName != "") {//if the line is not blank
+				players.add(playerName);//add that player to the list
+			} else {
+				break;
+			}
+		}
+		input.close();
+		
+		//return in the String[] format
+		String[] p = new String[players.size()];
+		return players.toArray(p);
+	}
 
 }
