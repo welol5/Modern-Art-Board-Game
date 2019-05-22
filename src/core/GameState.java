46c1be3809e29861566d6bc734ae77a3ec6b7bc5
Will Elliman <william.elliman@gmail.com>
2019-05-22 13:17:36 -0400

    GameState can now make all of the starting players

--- src/core/GameState.java
+++ src/core/GameState.java
@@ -2,7 +2,26 @@ package core;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author William Elliman
+ * 
+ * This class is used to hold the higher level data about the game, as well as data that no players know.
+ *
+ */
 public class GameState {
-	private ArrayList<Player> players;
+	private Player[] players;
+	private ArrayList<Card> deck;
 	
+	/**
+	 * This is used to setup a new game.
+	 * @param players
+	 */
+	public GameState(String[] players) {
+		//first all the new players need to be created
+		this.players = new Player[players.length];
+		for(int i = 0; i < players.length; i++) {
+			this.players[i] = new Player(players[i]);
+		}
+	}
 }
