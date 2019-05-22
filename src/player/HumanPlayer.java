06bb7758e1ab1c3b6b75e6cf0ba7d08cc9005653
Will Elliman <william.elliman@gmail.com>
2019-05-22 16:05:04 -0400

    Added the HumanPlayer class

---
+++ src/player/HumanPlayer.java
@@ -0,0 +1,16 @@
+package player;
+import core.Card;
+import core.Player;
+
+public class HumanPlayer extends Player{
+
+	public HumanPlayer(String name) {
+		super(name);
+	}
+
+	@Override
+	public Card chooseCard() {
+		return null;
+	}
+
+}
