e6dcd8770efc229a51a65be281f20e70e2e2a91a
Will Elliman <william.elliman@gmail.com>
2019-05-22 14:15:36 -0400

    Added toString method

--- src/core/Card.java
+++ src/core/Card.java
@@ -16,4 +16,40 @@ public class Card {
 	public AuctionType getAuctionType() {
 		return auctionType;
 	}
+	
+	public String toString() {
+		String string = "";
+		
+		//add the artist to the string
+		if(artist == Artist.CHRISTIN_P) {
+			string += "Christin P. : ";
+		} else if(artist == Artist.KARL_GITTER) {
+			string += "Karl Gitter : ";
+		} else if(artist == Artist.KRYPTO) {
+			string += "Krypto      : ";
+		} else if(artist == Artist.LITE_METAL) {
+			string += "Lite Metal  : ";
+		} else if(artist == Artist.YOKO) {
+			string += "Yoko        : ";
+		}
+		
+		//add the auction type to the string
+		if(auctionType == AuctionType.DOUBLE) {
+			string += "Double";
+		} else if(auctionType == AuctionType.FIXED_PRICE) {
+			string += "Fixed Price";
+		} else if(auctionType == AuctionType.ONCE_AROUND) {
+			string += "Once Around";
+		} else if(auctionType == AuctionType.SEALED) {
+			string += "Sealed";
+		} else if(auctionType == AuctionType.STANDARD) {
+			string += "Standard";
+		}
+		
+		if(string.equals("")) {
+			return "Unknown";
+		}
+		
+		return string;
+	}
 }
