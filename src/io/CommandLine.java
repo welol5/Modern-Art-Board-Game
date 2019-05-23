3e1a7ed928265f20373813f739a43806d12d2f22
Will Elliman <william.elliman@gmail.com>
2019-05-23 12:23:26 -0400

    Fixed a bug where players were not being created

--- src/io/CommandLine.java
+++ src/io/CommandLine.java
@@ -7,22 +7,20 @@ public class CommandLine extends BasicIO{
 
 	@Override
 	public String[] getPlayers() {
-		String playerName = null;//temporary storage of the current name of a player
-		ArrayList<String> players = new ArrayList<String>();//the names of all of the current players
+		String[] players;;//the names of all of the current players
 		Scanner input = new Scanner(System.in);//the input method for the names
 		System.out.print("How many players? (3 to 5 are allowed) : ");
 		int playerCount = input.nextInt();
 		input.nextLine();
+		players = new String[playerCount];
 		for(int i = 0; i < playerCount; i++) {
 			System.out.print("Enter a player's name: ");
-			playerName = input.nextLine();//get a players name
+			players[i] = input.nextLine();//get a players name
 			//System.out.println(playerName);//debug
 		}
 		input.close();
 		
-		//return in the String[] format
-		String[] p = new String[players.size()];
-		return players.toArray(p);
+		return players;
 	}
 
 	@Override
