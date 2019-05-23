2f8d9c8d4cedda1651acb5d9039b8d95f83bd8c1
Will Elliman <william.elliman@gmail.com>
2019-05-23 12:15:43 -0400

    Added end of the season tasks

--- src/core/GameDriver.java
+++ src/core/GameDriver.java
@@ -1,5 +1,6 @@
 package core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import io.BasicIO;
@@ -7,14 +8,14 @@ import io.CommandLine;
 import io.IOType;
 
 public class GameDriver implements Runnable{
-	
+
 	//IO var
 	private BasicIO io;
-	
+
 	//GameState var
 	GameState state;
 	Player[] players;
-	
+
 	/**
 	 * Setup the program by giving it a IO type.
 	 * IO types are public constant ints in the GameDriver class.
@@ -33,39 +34,60 @@ public class GameDriver implements Runnable{
 		//get the players list to use later
 		players = state.getPlayers();
 		int turn = 0;//keeps track of whose turn it is
-		
+
 		////////////////////////////////////////////////////////////////////
 		//everything past this point is incomplete
-		
+
 		//the game is now ready for the first season
 		for(int i = 1; i <= 4; i++) {
 			io.startSeason(i);
-			
+			//this is setup for the end of the season
+			Artist[] top3;
+
 			//deal out cards
 			for(int d = 0; d < state.dealAmounts[i]; d++) {
 				for(Player player : players) {
 					player.deal(state.drawCard());
 				}
 			}
-			
-			//TODO check if the season is over
+
 			for(;true;turn = (turn+1)%players.length) {
 				Card card = players[turn].chooseCard();
-				//the bidding can now begin
-				Bid winningBid = standardBidding(turn, card);
-				
-				//execute the bid
-				if(winningBid.index == turn) {
-					players[winningBid.index].pay(winningBid.price);
+				top3 = state.sell(card.getArtist());
+				if(top3 == null) {//this checks if the season is over by asking GameState
+					//the bidding can now begin
+					Bid winningBid = standardBidding(turn, card);
+
+					//execute the bid
+					if(winningBid.index == turn) {
+						players[winningBid.index].pay(winningBid.price);
+						players[winningBid.index].givePainting(card);
+					} else {
+						players[winningBid.index].pay(winningBid.price);
+						players[turn].recive(winningBid.price);
+						players[winningBid.index].givePainting(card);
+					}
 				} else {
-					players[winningBid.index].pay(winningBid.price);
-					players[turn].recive(winningBid.price);
+					//break out of the season once it is over
+					break;
 				}
 			}
+			
+			//the season is over by this point
+			for(Player player: players) {
+				ArrayList<Card> paintings = player.getWinnings();
+				for(Card card : paintings) {
+					if(top3[0] == card.getArtist() || top3[1] == card.getArtist() || top3[2] == card.getArtist()) {
+						player.recive(state.getArtistValue(card.getArtist()));
+					}
+				}
+				//reset the players winnings
+				player.clearWinnings();
+			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Runs the standard bidding option
 	 * @param turn the index of the player who played the card
@@ -77,7 +99,7 @@ public class GameDriver implements Runnable{
 		for(int i = 0; i < bidding.length; i++) {//all players are bidding
 			bidding[i] = true;
 		}
-		
+
 		int highestBid = 0;
 		int highestBidder = turn;
 		//while there is more than 1 player bidding
@@ -91,7 +113,7 @@ public class GameDriver implements Runnable{
 			if(hasWinner) {
 				break;
 			}
-			
+
 			//hasWinner will only be true if only one player has not backed out of bidding
 			for(int biddingTurn = 0; biddingTurn < players.length; biddingTurn++) {
 				int bid = players[(turn+biddingTurn)%players.length].getBid(card);
@@ -104,7 +126,7 @@ public class GameDriver implements Runnable{
 		}
 		return new Bid(highestBidder,highestBid);
 	}
-	
+
 	private class Bid{
 		public final int index,price;
 		public Bid(int index, int price) {
