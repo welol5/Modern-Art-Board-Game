3eaf77cb2656e32542808a7268c33b3643dd2e3f
Will Elliman <william.elliman@gmail.com>
2019-05-24 11:32:50 -0400

    Filled out RandomPlayers methods

--- src/player/RandomPlayer.java
+++ src/player/RandomPlayer.java
@@ -1,43 +1,71 @@
 package player;
 
+import java.util.Random;
+
 import core.Artist;
 import core.Card;
 
 public class RandomPlayer extends Player {
+	
+	Random random = new Random();
 
 	public RandomPlayer(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Card chooseCard() {
-		// TODO Auto-generated method stub
-		return null;
+		return hand.remove(random.nextInt(hand.size()));
 	}
 
 	@Override
 	public Card chooseSecondCard(Artist artist) {
-		// TODO Auto-generated method stub
-		return null;
+		//check if the hand contains the artist
+		boolean contains = false;
+		for(Card c : hand) {
+			if(c.getArtist() == artist) {
+				contains = true;
+				break;
+			}
+		}
+		if(!contains) {
+			return null;
+		}
+		
+		//the player has a card that will work, choose one randomly
+		Card card = null;
+		int index = 0;
+		while(card == null || card.getArtist() != artist) {
+			index = random.nextInt(hand.size());
+			card = hand.get(index);
+		}
+		hand.remove(index);
+		return card;
 	}
 
 	@Override
 	public int getBid(Card card, int highestSoFar) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(highestSoFar == -1) {
+			return random.nextInt(money);
+		} else if(highestSoFar > money){
+			return -1;
+		} else {
+			return random.nextInt(money-highestSoFar)+money;
+		}
 	}
 
 	@Override
 	public int getFixedPrice(Card card) {
-		// TODO Auto-generated method stub
-		return 0;
+		return random.nextInt(money);
 	}
 
 	@Override
 	public boolean buy(Card card, int price) {
-		// TODO Auto-generated method stub
-		return false;
+		if(price < money) {
+			return random.nextBoolean();
+		} else {
+			return false;
+		}
 	}
 
 }
