457a5a09afe5ce02163408889d200546bf61bf71
Will Elliman <william.elliman@gmail.com>
2019-05-29 14:07:22 -0400

    JavaDoc

--- src/core/Artist.java
+++ src/core/Artist.java
@@ -1,5 +1,9 @@
 package core;
-
+/**
+ * This enum hold the different artist names listed from highest to lowest value in terms of how many of each card is in the deck
+ * @author William Elliman
+ *
+ */
 public enum Artist {
 	LITE_METAL,
 	YOKO,
