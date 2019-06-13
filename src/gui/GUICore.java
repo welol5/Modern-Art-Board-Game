536d37f799b38bf47954be32569fd1fad7adfada
Will Elliman <william.elliman@gmail.com>
2019-06-13 11:31:31 -0400

    removed BasicIO

--- src/gui/GUICore.java
+++ src/gui/GUICore.java
@@ -25,7 +25,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import player.Player;
 
-public class GUICore extends Application implements BasicIO{
+public class GUICore extends Application{
 	
 	public static void main(String[] args) {
 		Application.launch(args);
@@ -42,89 +42,7 @@ public class GUICore extends Application implements BasicIO{
 		primaryStage.show();
 	}
 
-	@Override
-	public String[] getPlayers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void startSeason(int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void showHand(Player player, ArrayList<Card> hand) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void showHand(Player player, ArrayList<Card> hand, Artist artist) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getHandIndex(int maxVal) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getHandIndex(ArrayList<Card> hand, Artist artist) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void end() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getBid(String player, int money, int highestSoFar) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getFixedPrice(Card card) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean askPlayertoBuy(Card card, int price) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void auctionWinner(String name, int price) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void announceAuctionType(AuctionType type) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void announceCard(Card card) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void announceWinner(Player player) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 	
 	/////////////////////////////////////////////////
 	//below here are helper methods to show specific things
