c41f783557d448d4eaa4a66de63664bfabecbe14
Will Elliman <william.elliman@gmail.com>
2019-06-13 11:31:51 -0400

    Added a start to GamePane

---
+++ src/gui/GamePane.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import player.Player;
+
+public class GamePane extends GridPane {
+	
+	private Player player;
+
+	public GamePane(Player player) {
+		super();
+		this.player = player;
+	}
+	
+	private Node makeCardPanel() {
+		HBox cards = new HBox();
+		
+		return cards;
+	}
+}
