ecd9e2cce4ee56279c90406080b4b0968498a236
Will Elliman <william.elliman@gmail.com>
2019-07-15 14:24:47 -0400

    Added a way to play games vs. random sets of AIs

--- src/fxmlgui/GUIDriver.java
+++ src/fxmlgui/GUIDriver.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import core.Artist;
@@ -44,29 +45,31 @@ import player.ReactiveAIPlayer;
  *
  */
 public class GUIDriver implements Initializable, Observer{
-	
+
 	@FXML Button startGame;
-	
+
 	@FXML TextField player1Name;
 	@FXML TextField player2Name;
 	@FXML TextField player3Name;
 	@FXML TextField player4Name;
 	@FXML TextField player5Name;
-	
+
 	@FXML ComboBox<PlayerType> player1Type;
 	@FXML ComboBox<PlayerType> player2Type;
 	@FXML ComboBox<PlayerType> player3Type;
 	@FXML ComboBox<PlayerType> player4Type;
 	@FXML ComboBox<PlayerType> player5Type;
-	
+
+	@FXML TextField randPlayerName;
+
 	private PlayerView playerView;
 	private HBox playerfxml;
-	
+
 	private Service<Void> gameThread;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		for(PlayerType type : PlayerType.values()) {
 			player1Type.getItems().add(type);
 			player2Type.getItems().add(type);
@@ -74,28 +77,28 @@ public class GUIDriver implements Initializable, Observer{
 			player4Type.getItems().add(type);
 			player5Type.getItems().add(type);
 		}
-		
+
 	}
-	
+
 	@FXML private void startGame(ActionEvent event) {
-		
+
 		//get the stage so the scene can be changed
 		Stage mainStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-		
+
 		//change the scene
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("PlayerView.fxml"));
 			playerfxml = loader.load();
 			playerView = (PlayerView)loader.getController();
 			mainStage.setScene(new Scene(playerfxml));
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.err.println("PlayerView.fxml file is missing or corrupted");
 			System.exit(0);
 		}
-		
+
 		//get all the names and types
 		ArrayList<String> names = new ArrayList<String>();
 		ArrayList<PlayerType> types = new ArrayList<PlayerType>();
@@ -119,34 +122,34 @@ public class GUIDriver implements Initializable, Observer{
 			names.add(player5Name.getText());
 			types.add(player5Type.getValue());
 		}
-		
+
 		//make the game states that the players need to see
 		GameState state = new GameState(names.size());
 		ObservableGameState OGS = new ObservableGameState(state);
-		
+
 		playerView.bindPrices(
 				OGS.getArtistValueString(Artist.LITE_METAL),
 				OGS.getArtistValueString(Artist.YOKO),
 				OGS.getArtistValueString(Artist.CHRISTIN_P),
 				OGS.getArtistValueString(Artist.KARL_GITTER),
 				OGS.getArtistValueString(Artist.KRYPTO));
-		
+
 		playerView.bindCounts(
 				OGS.getArtistCountString(Artist.LITE_METAL),
 				OGS.getArtistCountString(Artist.YOKO),
 				OGS.getArtistCountString(Artist.CHRISTIN_P),
 				OGS.getArtistCountString(Artist.KARL_GITTER),
 				OGS.getArtistCountString(Artist.KRYPTO));
-		
+
 		Player[] players = makePlayers(names,types,OGS);
 		if(!playerView.isSet()) {
 			playerView.setPlayer(players[0]);
 		}
-		
+
 		playerView.addOtherPlayers(players);
-		
+
 		gameThread = new Service<Void>() {
-			
+
 			protected Task<Void> createTask(){
 				return new Task<Void>() {
 
@@ -156,21 +159,105 @@ public class GUIDriver implements Initializable, Observer{
 						//the game starts now
 						GameDriver driver = new GameDriver(players, state, OGS);
 						driver.playGame();
-						
+
 						return null;
 					}
 				};
 			}
 		};
-		
-		
-		
+
 		gameThread.restart();
 	}
-	
+
+	@FXML private void startRandomGame(ActionEvent event) {
+
+		//get the stage so the scene can be changed
+		Stage mainStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+		//change the scene
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("PlayerView.fxml"));
+			playerfxml = loader.load();
+			playerView = (PlayerView)loader.getController();
+			mainStage.setScene(new Scene(playerfxml));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("PlayerView.fxml file is missing or corrupted");
+			System.exit(0);
+		}
+
+		Random random = new Random();
+		int playerCount = random.nextInt(4);//can have up to 4 other players
+
+		ArrayList<String> names = new ArrayList<String>();
+		ArrayList<PlayerType> types = new ArrayList<PlayerType>();
+
+		for(int i = 0; i < playerCount; i++) {
+			names.add("p" + i);
+			//get a random playerType
+			//the "+- 1" excludes human and genetic
+			types.add(PlayerType.values()[random.nextInt(1+PlayerType.values().length-2)]);
+		}
+
+		int playerIndex = random.nextInt(playerCount);
+		names.add(playerIndex, randPlayerName.getText());
+		types.add(playerIndex, PlayerType.HUMAN);
+		
+		for(PlayerType type : types) {
+			System.out.println(type);
+		}
+
+		//make the game states that the players need to see
+		GameState state = new GameState(names.size());
+		ObservableGameState OGS = new ObservableGameState(state);
+
+		playerView.bindPrices(
+				OGS.getArtistValueString(Artist.LITE_METAL),
+				OGS.getArtistValueString(Artist.YOKO),
+				OGS.getArtistValueString(Artist.CHRISTIN_P),
+				OGS.getArtistValueString(Artist.KARL_GITTER),
+				OGS.getArtistValueString(Artist.KRYPTO));
+
+		playerView.bindCounts(
+				OGS.getArtistCountString(Artist.LITE_METAL),
+				OGS.getArtistCountString(Artist.YOKO),
+				OGS.getArtistCountString(Artist.CHRISTIN_P),
+				OGS.getArtistCountString(Artist.KARL_GITTER),
+				OGS.getArtistCountString(Artist.KRYPTO));
+
+		Player[] players = makePlayers(names,types,OGS);
+		if(!playerView.isSet()) {
+			playerView.setPlayer(players[0]);
+		}
+
+		playerView.addOtherPlayers(players);
+
+		gameThread = new Service<Void>() {
+
+			protected Task<Void> createTask(){
+				return new Task<Void>() {
+
+					@Override
+					protected Void call() throws Exception {
+
+						//the game starts now
+						GameDriver driver = new GameDriver(players, state, OGS);
+						driver.playGame();
+
+						return null;
+					}
+				};
+			}
+		};
+
+		gameThread.restart();
+	}
+
 	/////////////////////////////////////////////
 	//helper stuff
-	
+
 	private PlayerType getPlayerType(String type) {
 		if(type == null || type.equalsIgnoreCase("Random")) {
 			return PlayerType.RANDOM;
@@ -185,14 +272,14 @@ public class GUIDriver implements Initializable, Observer{
 		} else if(type.equalsIgnoreCase("Basic Predictive V2")) {
 			return PlayerType.BASIC_PREDICTIVE_AI_V2;
 		}
-		
+
 		//if all else fails, return random
 		return PlayerType.RANDOM;
 	}
-	
+
 	private Player[] makePlayers(ArrayList<String> names, ArrayList<PlayerType> types, ObservableGameState OGS) {
 		Player[] players = new Player[names.size()];
-		
+
 		for(int i = 0; i < players.length; i++) {
 			boolean add = true;
 			if(types.get(i) == PlayerType.HUMAN) {
@@ -220,12 +307,12 @@ public class GUIDriver implements Initializable, Observer{
 				players[i] = new RandomPlayer(names.get(i));
 			}
 		}
-		
+
 		return players;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		
+
 	}
 }
