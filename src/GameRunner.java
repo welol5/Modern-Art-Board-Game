d650e7300260299559a0a573b7387f0767c8d2ad
Will Elliman <william.elliman@gmail.com>
2019-07-15 16:05:27 -0400

    Fixed tournament games to not include useless games

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
+import core.Artist;
+import core.GameState;
+import core.ObservableGameState;
 import fxmlgui.GameDriver;
+import player.BasicPredictiveAIPlayer;
+import player.BasicPredictiveAIPlayerV2;
+import player.HandStateCardPicker;
+import player.HighRoller;
+import player.MemoryAIPlayer;
+import player.Merchant;
 import player.Player;
+import player.PlayerType;
+import player.RandomPlayer;
+import player.ReactiveAIPlayer;
 
 public class GameRunner extends Thread{
 
@@ -7,6 +21,9 @@ public class GameRunner extends Thread{
 	private boolean done = false;
 	private final int timeout;
 	private GameDriver driver;
+	
+	private ArrayList<PlayerType> types = null;
+	private ArrayList<String> names;
 	
 	private boolean stop = false;
 
@@ -21,14 +38,30 @@ public class GameRunner extends Thread{
 		while(!stop) {
 			done = false;
 			
-			//get a game
-			driver = TournamentGame.getGameDriver();
-//			System.out.println(driver);
-			if(driver == null) {
+			//get a list of players
+			types = TournamentGame.getPlayerList();
+
+			if(types == null) {
 				continue;
 			}
 
 			while(!done) {
+				
+				//make the game
+				GameState state = new GameState(types.size());
+				ObservableGameState OGS = new ObservableGameState(state);
+				
+				//make the players
+				names = new ArrayList<String>();
+				for(PlayerType type : types) {
+					names.add(type.toString());
+				}
+				Player[] players = makePlayers(names, types, OGS);
+				
+				//make the driver
+				GameDriver driver = new GameDriver(players, state, OGS, false);
+				
+				//make the thread to run the game
 				Thread runner = new Thread(new Runnable() {
 
 					@Override
@@ -47,17 +80,41 @@ public class GameRunner extends Thread{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				if(!done) {
-					driver.resetGame();
-				}
 			}
-
 			TournamentGame.addWin(winner);
 		}
 	}
 	
 	public void stopRunner() {
 		stop = true;
+	}
+	
+	private static Player[] makePlayers(ArrayList<String> names, ArrayList<PlayerType> types, ObservableGameState OGS) {
+		Player[] players = new Player[names.size()];
+
+		for(int i = 0; i < players.length; i++) {
+			boolean add = true;
+			if(types.get(i) == PlayerType.RANDOM) {
+				players[i] = new RandomPlayer(names.get(i));
+			} else if(types.get(i) == PlayerType.REACTIVE_AI) {
+				players[i] = new ReactiveAIPlayer(names.get(i),OGS);
+			} else if(types.get(i) == PlayerType.MEMORY_AI) {
+				players[i] = new MemoryAIPlayer(names.get(i),OGS, players.length,i);
+			} else if(types.get(i) == PlayerType.BASIC_PREDICTIVE_AI) {
+				players[i] = new BasicPredictiveAIPlayer(names.get(i),OGS, players.length,i);
+			} else if(types.get(i) == PlayerType.BASIC_PREDICTIVE_AI_V2) {
+				players[i] = new BasicPredictiveAIPlayerV2(names.get(i),OGS, players.length,i);
+			} else if(types.get(i) == PlayerType.HIGH_ROLLER) {
+				players[i] = new HighRoller(names.get(i),OGS, players.length,i);
+			} else if(types.get(i) == PlayerType.MERCHANT) {
+				players[i] = new Merchant(names.get(i),OGS, players.length,i);
+			} else if(types.get(i) == PlayerType.HAND_STATE_CARD_PICKER) {
+				players[i] = new HandStateCardPicker(names.get(i),OGS, players.length,i);
+			} else {
+				players[i] = new RandomPlayer(names.get(i));
+			}
+		}
+
+		return players;
 	}
 }
--- src/TournamentGame.java
+++ src/TournamentGame.java
@@ -18,12 +18,12 @@ import player.ReactiveAIPlayer;
 
 public class TournamentGame {
 
-	private static final int trials = 100;
+	private static final int trials = 1;
 	private static final int timeout = 10000;
 	
 	private static final int threadCount = 8;
 
-	private static ArrayList<GameDriver> allGames = new ArrayList<GameDriver>();
+	private static ArrayList<ArrayList<PlayerType>> allGames = new ArrayList<ArrayList<PlayerType>>();
 	private static HashMap<String,Integer> wins;
 
 	public static void main(String[] args) {
@@ -38,20 +38,10 @@ public class TournamentGame {
 		playerTypeList.add(PlayerType.MERCHANT);
 		playerTypeList.add(PlayerType.HAND_STATE_CARD_PICKER);
 
-		ArrayList<String> nameList = new ArrayList<String>();
-		nameList.add("Random");
-		nameList.add("Reactive");
-		nameList.add("Memory");
-		nameList.add("V1");
-		nameList.add("V2");
-		nameList.add("HighRoller");
-		nameList.add("Merchant");
-		nameList.add("HSCPAI");
-
 		wins = new HashMap<String,Integer>();
 		//init to 0s
-		for(String s : nameList) {
-			wins.put(s, 0);
+		for(PlayerType type : playerTypeList) {
+			wins.put(type.toString(), 0);
 		}
 
 		int iterationIndex = 0;
@@ -61,47 +51,16 @@ public class TournamentGame {
 			for(int playerTwoSlot = 0; playerTwoSlot < playerTypeList.size(); playerTwoSlot++) {
 				for(int playerThreeSlot = 0; playerThreeSlot < playerTypeList.size(); playerThreeSlot++) {
 
-					ArrayList<PlayerType> currentPlayerTypes = new ArrayList<PlayerType>();
-					currentPlayerTypes.add(playerTypeList.get(playerOneSlot));
-					currentPlayerTypes.add(playerTypeList.get(playerTwoSlot));
-					currentPlayerTypes.add(playerTypeList.get(playerThreeSlot));
-
-					ArrayList<String> currentPlayerNames = new ArrayList<String>();
-					currentPlayerNames.add(nameList.get(playerOneSlot));
-					currentPlayerNames.add(nameList.get(playerTwoSlot));
-					currentPlayerNames.add(nameList.get(playerThreeSlot));
-
 					//run each order for (trials) iterations
 					for(int i = 0; i < trials; i++) {
-						GameState state = new GameState(3);
-						ObservableGameState OGS = new ObservableGameState(state);
-						Player[] players = makePlayers(currentPlayerNames, currentPlayerTypes, OGS);
-						GameDriver driver = new GameDriver(players,state,OGS, false);
+						
+						ArrayList<PlayerType> currentPlayerTypes = new ArrayList<PlayerType>();
+						currentPlayerTypes.add(playerTypeList.get(playerOneSlot));
+						currentPlayerTypes.add(playerTypeList.get(playerTwoSlot));
+						currentPlayerTypes.add(playerTypeList.get(playerThreeSlot));
 
-						allGames.add(driver);
+						allGames.add(currentPlayerTypes);
 
-						//						GameRunner runner = new GameRunner(driver,timeout);
-						//						Thread gameThread = new Thread(runner);
-						//						gameThread.start();
-						//						try {
-						//							gameThread.join(timeout);
-						//						} catch (InterruptedException e) {
-						//							// TODO Auto-generated catch block
-						//							e.printStackTrace();
-						//						}
-						//
-						//						Player winner;
-						//						if(runner.done) {
-						//							winner = runner.winner;
-						//						} else {
-						//							i--;
-						//							continue;
-						//							//if this happened the game failed and shoulf be redone
-						//						}
-						//
-						//						wins.put(winner.name, wins.get(winner.name) + 1);
-						//						iterationIndex++;
-						//						System.out.println(iterationIndex + " / " + totalIterations);
 					}
 				}
 			}
@@ -142,38 +101,9 @@ public class TournamentGame {
 		}
 		
 		//print results
-		for(String name : nameList) {
-			System.out.println(name + " :: " + wins.get(name));
+		for(PlayerType type : playerTypeList) {
+			System.out.println(type + " :: " + wins.get(type.toString()));
 		}
-	}
-
-	private static Player[] makePlayers(ArrayList<String> names, ArrayList<PlayerType> types, ObservableGameState OGS) {
-		Player[] players = new Player[names.size()];
-
-		for(int i = 0; i < players.length; i++) {
-			boolean add = true;
-			if(types.get(i) == PlayerType.RANDOM) {
-				players[i] = new RandomPlayer(names.get(i));
-			} else if(types.get(i) == PlayerType.REACTIVE_AI) {
-				players[i] = new ReactiveAIPlayer(names.get(i),OGS);
-			} else if(types.get(i) == PlayerType.MEMORY_AI) {
-				players[i] = new MemoryAIPlayer(names.get(i),OGS, players.length,i);
-			} else if(types.get(i) == PlayerType.BASIC_PREDICTIVE_AI) {
-				players[i] = new BasicPredictiveAIPlayer(names.get(i),OGS, players.length,i);
-			} else if(types.get(i) == PlayerType.BASIC_PREDICTIVE_AI_V2) {
-				players[i] = new BasicPredictiveAIPlayerV2(names.get(i),OGS, players.length,i);
-			} else if(types.get(i) == PlayerType.HIGH_ROLLER) {
-				players[i] = new HighRoller(names.get(i),OGS, players.length,i);
-			} else if(types.get(i) == PlayerType.MERCHANT) {
-				players[i] = new Merchant(names.get(i),OGS, players.length,i);
-			} else if(types.get(i) == PlayerType.HAND_STATE_CARD_PICKER) {
-				players[i] = new HandStateCardPicker(names.get(i),OGS, players.length,i);
-			} else {
-				players[i] = new RandomPlayer(names.get(i));
-			}
-		}
-
-		return players;
 	}
 
 	public static void addWin(Player winner) {
@@ -182,7 +112,7 @@ public class TournamentGame {
 		}
 	}
 	
-	public static GameDriver getGameDriver() {
+	public static ArrayList<PlayerType> getPlayerList() {
 		synchronized(allGames) {
 			try {
 				return allGames.remove(0);
