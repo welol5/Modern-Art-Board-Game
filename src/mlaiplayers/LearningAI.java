fb39f50c78e6aa84abf8fdd51558691bc4d91fd3
William Elliman <william.elliman@gmail.com>
2019-09-18 18:24:08 -0400

    Changed to an interface

--- src/mlaiplayers/LearningAI.java
+++ src/mlaiplayers/LearningAI.java
@@ -2,10 +2,6 @@ package mlaiplayers;
 
 import player.Player;
 
-public abstract class LearningAI extends Player{
-
-	public LearningAI(String name) {
-		super(name);
-	}
-	
+public interface LearningAI{
+	public void learn();
 }
